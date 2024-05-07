3b6c17bac86b6c4955cffcd056a832af14c7b1e2
“Lotizzle <lotharmbaegbu@gmail.com>
2024-05-07 04:34:14 +0000

    Pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -10,30 +10,32 @@ from typing import List
 
 
 def index_range(page: int, page_size: int) -> tuple:
-        """
-        Return a tuple containing the start index and end index
-        corresponding to the range of indexes to return for pagination.
+    """
+    Return a tuple containing the start index and end index
+    corresponding to the range of indexes to return for pagination.
 
-        Page numbers are 1-indexed.
+    Page numbers are 1-indexed.
 
-        Args:
-            page (int): The page number.
-            page_size (int): The number of items per page.
+    Args:
+        page (int): The page number.
+        page_size (int): The number of items per page.
 
-        Returns:
-            tuple: A tuple containing the start index and end index.
-        """
+    Returns:
+        tuple: A tuple containing the start index and end index.
+    """
 
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page_size > 0
 
-        start_index = (page-1) * page_size
-        end_index = page * page_size
-        return start_index, end_index
+    start_index = (page-1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
     """
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -61,7 +63,7 @@ class Server:
         Returns:
             List[List]: The list of rows corresponding to the requested page.
         """
-        
+
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
         return dataset[start_index:end_index]
