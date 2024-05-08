bb8b78dbda7d85cf3210164dcd9481bf3b56a1e0
“Lotizzle <lotharmbaegbu@gmail.com>
2024-05-08 23:17:53 +0000

    Implemented LIFO

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains the class LIFOCache that inherits
+from BaseCaching and is a caching system.
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    This class implements the LIFO approach for caching
+    """
+
+    def __init__(self):
+        """Initializes"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                last_key = list(self.cache_data.keys())[-1]
+                print("DISCARD:{}".format(last_key))
+                del(self.cache_data[last_key])
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieves the item for a key """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
