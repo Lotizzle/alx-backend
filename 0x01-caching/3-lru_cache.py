69ac1d0174ec14dfba8ce48a402af0c8bcee8b46
“Lotizzle <lotharmbaegbu@gmail.com>
2024-05-08 23:47:09 +0000

    Implemented LRU

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 """
-This module contains the class LIFOCache that inherits
-from BaseCaching and is a caching system.
+This module contains a a class LRUCache that inherits
+from BaseCaching and is a caching system
 """
 
 from base_caching import BaseCaching
 from collections import OrderedDict
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """
-    This class implements the LIFO approach for caching
+    This class implements the LRU approach for caching
     """
 
     def __init__(self):
         """Initializes"""
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                last_key = next(reversed(self.cache_data))
-                print("DISCARD:{}".format(last_key))
-                self.cache_data.popitem(last=False)
+                discarded_key, _ = self.cache_data.popitem(last=False)
+                print("DISCARD:{}".format(discarded_key))
             self.cache_data[key] = item
 
     def get(self, key):
         """ Retrieves the item for a key """
         if key is not None and key in self.cache_data:
+            # Move the accessed item to the end of the OrderedDict
+            self.cache_data.move_to_end(key)
             return self.cache_data[key]
         return None
