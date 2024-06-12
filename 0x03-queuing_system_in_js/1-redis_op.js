bf634ab19711dace411d7fd1d5897e154050a74f
root <root@DESKTOP-9B16HS6>
2024-06-12 10:26:49 +0100

    Added new school

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+// 1-redis_op.js
+
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) =>  {
+  client.get(schoolName, (error, reply) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(reply);
+    }
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
